20a86f8af0c7ad76f9c129c252ebc0be0e9ea1e6
paddycull <paddycull9@hotmail.com>
2020-06-04 17:19:09 +0100

    Now sets PowerShell MaxMem as well as base setting.

--- Private/Set-SPMaxMemoryPerShellMB.ps1
+++ Private/Set-SPMaxMemoryPerShellMB.ps1
@@ -12,24 +12,34 @@ function Set-SPMaxMemoryPerShellMB {
 
     Connect-WSMan -ComputerName $TargetServer
     $CurrentMaxMemoryPerShellMB = Get-Item WSMan:\$TargetServer\Shell\MaxMemoryPerShellMB
+    $CurrentPowerShellMaxMemoryPerShellMB = Get-Item WSMan:\$TargetServer\Plugin\microsoft.powershell\Quotas\MaxMemoryPerShellMB
+
 
     $OldMaxMemory = [PSCustomObject][Ordered] @{
         ValueType = "CurrentValue"
         Property = $CurrentMaxMemoryPerShellMB.Name
-        Value = $CurrentMaxMemoryPerShellMB.Value
+        ShellValue = $CurrentMaxMemoryPerShellMB.Value
+        PowerShellValue = $CurrentPowerShellMaxMemoryPerShellMB.Value
     }
     $MaxMemorySettings += $OldMaxMemory
 
-    if($CurrentMaxMemoryPerShellMB.Value -ne $MaxMemoryPerShellMB) {
+    if($CurrentMaxMemoryPerShellMB.Value -ne $MaxMemoryPerShellMB -or $CurrentPowerShellMaxMemoryPerShellMB.Value -ne $MaxMemoryPerShellMB) {
         Set-Item  WSMan:\$TargetServer\Shell\MaxMemoryPerShellMB $MaxMemoryPerShellMB -Force -WarningAction SilentlyContinue
+        Set-Item  WSMan:\$TargetServer\Plugin\microsoft.powershell\Quotas\MaxMemoryPerShellMB $MaxMemoryPerShellMB -Force -WarningAction SilentlyContinue
 
         $NewMaxMemoryPerShellMB = Get-Item WSMan:\$TargetServer\Shell\MaxMemoryPerShellMB
+        $NewPowerShellMaxMemoryPerShellMB = Get-Item WSMan:\$TargetServer\Plugin\microsoft.powershell\Quotas\MaxMemoryPerShellMB
+
         $NewMaxMemory = [PSCustomObject][Ordered] @{
             ValueType = "NewValue"
             Property = $NewMaxMemoryPerShellMB.Name
-            Value = $NewMaxMemoryPerShellMB.Value
+            ShellValue = $NewMaxMemoryPerShellMB.Value
+            PowerShellValue = $NewPowerShellMaxMemoryPerShellMB.Value
         }
         $MaxMemorySettings += $NewMaxMemory
+
+        Write-Verbose "Restarting WinRM for changes to take effect"
+        Get-Service -ComputerName $TargetServer -Name WinRM | Restart-Service -Force
     }
     Disconnect-WSMan -ComputerName $TargetServer
 
